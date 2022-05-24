c0a9a934c45b5fbd4e3dc2b8877e4adee8fafe56
Michael Ferraro <mf69386p@pace.edu>
2022-05-23 21:42:23 -0400

    practice leetcode

--- src/practiceEnv/addTwoNumbers/addTwoNumbersSource.java
+++ src/practiceEnv/addTwoNumbers/addTwoNumbersSource.java
@@ -1,17 +1,36 @@
 package practiceEnv.addTwoNumbers;
 
+import java.util.Arrays;
+
 public class addTwoNumbersSource {
 
     public addTwoNumbersSource() {
-        ListNode num3 = new ListNode(3);
+        ListNode num3 = new ListNode(2);
         ListNode num2 = new ListNode(4, num3);
-        ListNode l1 = new ListNode(2, num2);
+        ListNode l1 = new ListNode(3, num2);
 
-        ListNode newNum3 = new ListNode(4);
+        ListNode newNum3 = new ListNode(5);
         ListNode newNum2 = new ListNode(6, newNum3);
-        ListNode l2 = new ListNode(5, newNum2);
+        ListNode l2 = new ListNode(4, newNum2);
 
-        addTwoNumbers(l1, l2);
+        ListNode returned = addTwoNumbers(l1, l2);
+        int[] corrected = new int[3];
+        int i = 0;
+        while (returned!=null){
+            corrected[i]= returned.val;
+            i++;
+            returned = returned.next;
+        }
+        for (int j = 0; j < corrected.length-1; j++){
+            int temp = corrected[j];
+            corrected[j] = corrected[corrected.length-1-j];
+            corrected[corrected.length-1-j] = temp;
+        }
+        StringBuilder answer = new StringBuilder();
+        for (int j : corrected) {
+            answer.append(j);
+        }
+        System.out.println(answer);
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
