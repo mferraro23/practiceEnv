32cdd9806dab437b99880941976639656dbeb092
Michael Ferraro <mf69386p@pace.edu>
2022-05-29 00:35:54 -0400

    LeetCode Practice (finished addTwoNumbers)

--- src/practiceEnv/addTwoNumbers/addTwoNumbersSource.java
+++ src/practiceEnv/addTwoNumbers/addTwoNumbersSource.java
@@ -3,15 +3,21 @@ package practiceEnv.addTwoNumbers;
 public class addTwoNumbersSource {
 
     public addTwoNumbersSource() {
+        // Declare ListNodes
+        // Each Number is stored in Linked List
+        // This number is 342.
         ListNode num3 = new ListNode(2);
         ListNode num2 = new ListNode(4, num3);
         ListNode l1 = new ListNode(3, num2);
-
+        // This number is 465
         ListNode newNum3 = new ListNode(5);
         ListNode newNum2 = new ListNode(6, newNum3);
         ListNode l2 = new ListNode(4, newNum2);
 
+        //Returned value of the two numbers summed together in reverse.
         ListNode returned = addTwoNumbers(l1, l2);
+
+        // Create new array to reverse it back to normal
         int[] corrected = new int[3];
         int i = 0;
         while (returned!=null){
@@ -37,6 +43,8 @@ public class addTwoNumbersSource {
         ListNode q = l2;
         ListNode current = dummyHead;
         int carry = 0;
+
+        // Make sure either are null, continue with arithmetic inside the linked list.
         while (p != null || q != null) {
             int x = (p!=null) ? p.val : 0;
             int y = (q!=null) ? q.val : 0;
