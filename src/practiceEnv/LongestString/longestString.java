24e1beba78cfce6154b150e78f62e23f6687def1
Michael Ferraro <mf69386p@pace.edu>
2022-05-29 00:29:31 -0400

    LeetCode Practice (finished LongestString)

--- src/practiceEnv/LongestString/longestString.java
+++ src/practiceEnv/LongestString/longestString.java
@@ -6,11 +6,15 @@ import java.util.Set;
 
 public class longestString {
     public longestString(){
+        // Declare String
         String s = "abcabcbb";
+
+        // Returns integer value of the longest string (non-repeating)
         System.out.println(lengthOfLongestString(s));
     }
     public int lengthOfLongestString(String s){
         int max = 0;
+        // Base case, if string is empty return 0
         if (Objects.isNull(s) || "".equals(s)) {
             return max;
         }
