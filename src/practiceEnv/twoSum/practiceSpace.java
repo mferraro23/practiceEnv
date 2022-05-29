0db88fe4f9fd929c48e4ee702095540fb8123a7f
Michael Ferraro <mf69386p@pace.edu>
2022-05-29 00:23:28 -0400

    LeetCode Practice (finished twoSum)

--- src/practiceEnv/twoSum/practiceSpace.java
+++ src/practiceEnv/twoSum/practiceSpace.java
@@ -5,15 +5,20 @@ import java.util.Arrays;
 public class practiceSpace {
 
     public practiceSpace() {
-        int[] newArray = {3,2,4};
-        int target = 6;
-        int[] solution = twoSum(newArray, target);
-        System.out.println(Arrays.toString(solution));
+        // Declare the array.
+        int[] newArray = {3,2,7};
+        // target shows what sum we want to end up with.
+        int target = 10;
+        // will return the indicies that sum to target.
+        System.out.println(Arrays.toString(twoSum(newArray, target)));
     }
     public int[] twoSum(int[] nums, int target) {
+        // new array to add the proper indices to
         int[] newArray = new int[2];
         int j = 0;
         boolean reached = false;
+
+        // while it hasn't been reached we can check all indices.
         while(!reached){
             for(int i = 0; i<nums.length-1;i++){
                 if(nums[i]+nums[j] == target){
@@ -26,7 +31,6 @@ public class practiceSpace {
             }
             j += 1;
         }
-
         return newArray;
     }
 }
