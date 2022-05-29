79b6a01bcab3abbaf70e4a56bf400dd1f1f6bd54
Michael Ferraro <mf69386p@pace.edu>
2022-05-28 23:35:32 -0400

    LeetCode Practice

--- src/practiceEnv/ZigzagConversion/ZigzagClass.java
+++ src/practiceEnv/ZigzagConversion/ZigzagClass.java
@@ -4,9 +4,43 @@ public class ZigzagClass {
     public ZigzagClass(){
         int rows = 3;
         String s = "PAYPALISHIRING";
+
         System.out.println(ZigzagRun(rows, s));
     }
     public String ZigzagRun(int rows, String s){
-        return s;
+        StringBuilder newString = new StringBuilder();
+        int currentRow = 0;
+        while(currentRow != rows){
+            if (currentRow == rows-1){
+                newString.append("\n");
+                for(int i = 0; i <s.length(); i++){
+                    if (i % 2 == 0 && i % 4 != 0) {
+                        newString.append(s.charAt(i));
+                        newString.append("   ");
+                    }
+                }
+            }
+            else if (currentRow % 2 == 0){
+                newString.append("\n");
+                for(int i = 0; i <s.length(); i++){
+                    if (i % 4 == 0) {
+                        newString.append(s.charAt(i));
+                        newString.append("   ");
+                    }
+                }
+            }
+            else{
+                newString.append("\n");
+                for(int i = 0; i <s.length(); i++){
+                    if (i % 2 == 1){
+                        newString.append(s.charAt(i));
+                        newString.append(" ");
+                    }
+                }
+            }
+            currentRow += 1;
+        }
+
+        return newString.toString();
     }
 }
