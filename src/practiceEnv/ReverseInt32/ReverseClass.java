40c92f20130d7d0891f6efbe3d39453249ce8b0b
Michael Ferraro <mf69386p@pace.edu>
2022-05-29 23:14:30 -0400

    LeetCode Practice (finished ReverseInt32)

---
+++ src/practiceEnv/ReverseInt32/ReverseClass.java
@@ -0,0 +1,25 @@
+package practiceEnv.ReverseInt32;
+
+public class ReverseClass {
+    public ReverseClass() {
+        int x = -145;
+        System.out.println(reverse(x));
+    }
+    public int reverse(int x){
+        // Create String value |x|
+        StringBuilder reverseInt = new StringBuilder(String.valueOf(Math.abs(x)));
+
+        // If x<0 add negative sign to end
+        if(x<0){
+            reverseInt.append("-");
+        }
+        reverseInt.reverse();
+        // Catches 32bit unsigned and signed exception
+        try{
+            return Integer.parseInt(reverseInt.toString());
+        }
+        catch(Exception e){
+            return 0;
+        }
+    }
+}
---
+++ src/practiceEnv/ReverseInt32/ReverseDriver.java
@@ -0,0 +1,7 @@
+package practiceEnv.ReverseInt32;
+
+public class ReverseDriver {
+    public static void main(String[] args) {
+        new ReverseClass();
+    }
+}
