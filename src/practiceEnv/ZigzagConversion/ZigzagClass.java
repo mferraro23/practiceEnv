cd1e0c1ba35297c61be472c7b9131bae290535c2
Michael Ferraro <mf69386p@pace.edu>
2022-05-29 21:55:29 -0400

    LeetCode Practice (finished addTwoNumbers)

--- src/practiceEnv/ZigzagConversion/ZigzagClass.java
+++ src/practiceEnv/ZigzagConversion/ZigzagClass.java
@@ -1,5 +1,7 @@
 package practiceEnv.ZigzagConversion;
 
+import java.util.Arrays;
+
 public class ZigzagClass {
     public ZigzagClass(){
         int rows = 3;
@@ -8,47 +10,47 @@ public class ZigzagClass {
         System.out.println(ZigzagRun(rows, s));
     }
     public String ZigzagRun(int rows, String s){
-        // only works with rows = 3
-        // fix for more rows
-        // --------------------------- //
-        // Create StringBuilderList
-        // For each character index add to corresponding array index
-        // Once index rows is reached head back up the StringBuilderList
-        // Continue Zigzag pattern
-        // Combine all StringBuilderLists into one StringBuilder, return that StringBuilder.toString().
-        StringBuilder newString = new StringBuilder();
-        int currentRow = 0;
-        while(currentRow != rows){
-            if (currentRow == rows-1){
-                newString.append("\n");
-                for(int i = 0; i <s.length(); i++){
-                    if (i % 2 == 0 && i % 4 != 0) {
-                        newString.append(s.charAt(i));
-                        newString.append("   ");
-                    }
-                }
-            }
-            else if (currentRow % 2 == 0){
-                newString.append("\n");
-                for(int i = 0; i <s.length(); i++){
-                    if (i % 4 == 0) {
-                        newString.append(s.charAt(i));
-                        newString.append("   ");
-                    }
-                }
-            }
-            else{
-                newString.append("\n");
-                for(int i = 0; i <s.length(); i++){
-                    if (i % 2 == 1){
-                        newString.append(s.charAt(i));
-                        newString.append(" ");
-                    }
-                }
-            }
-            currentRow += 1;
+        // Base Case
+        if (rows <= 1){
+            return s;
         }
+        // Declare StringBuilder Array
+        StringBuilder[] newString = new StringBuilder[rows];
+        // Init newStringBuilder for each index
+        for (int l = 0; l < rows; l++){
+            newString[l] = new StringBuilder();
+        }
+        // Declare finalString to append all indices to
+        StringBuilder finalString = new StringBuilder();
 
-        return newString.toString();
+        // Direction = 0 means we are going 0->rows
+        int direction = 0;
+        for(int i=0, j=0; i<s.length();i++){
+            // If at the bottom then append in ascending order
+            if (direction == 0){
+                boolean top = j>=rows-1;
+                newString[j].append(s.charAt(i));
+                if(top){
+                    // Direction = 1 means we are going rows->0
+                    direction = 1;
+                }
+                // Check if we are at the top
+                j = top ? (j-1):(j+1);
+            }
+            // If at the top then append in descending order
+            else{
+                boolean bottom = j<=0;
+                newString[j].append(s.charAt(i));
+                if(bottom){
+                    direction = 0;
+                }
+                j = bottom ? (j+1):(j-1);
+            }
+        }
+        // Append each StringBuilder index to finalString
+        for(StringBuilder l: newString){
+            finalString.append(l);
+        }
+        return finalString.toString();
     }
 }
