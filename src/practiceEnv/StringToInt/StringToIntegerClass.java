72a1e85df9845d324b998980a7fd9c596a0558bd
Michael Ferraro <mf69386p@pace.edu>
2022-05-30 23:44:35 -0400

    LeetCode Practice (continued work on StringToInt)

--- src/practiceEnv/StringToInt/StringToIntegerClass.java
+++ src/practiceEnv/StringToInt/StringToIntegerClass.java
@@ -2,38 +2,56 @@ package practiceEnv.StringToInt;
 
 public class StringToIntegerClass {
     public StringToIntegerClass(){
-        String s = "                  -123bncnwixnwx";
+        String s = "-298239483957349578734739473947937";
         System.out.println(toInt(s));
     }
     public int toInt(String s){
+        String integerString = s.strip();
         //-----------------Base Case-----------------
+        if(integerString.isEmpty()){
+            return 0;
+        }
+        else if(integerString.charAt(0) != '-'){
+            if(!Character.isDigit(integerString.charAt(0))){
+                return 0;
+            }
+        }
         int count = 0;
-        for(int i = 0; i<s.length(); i++){
-            if(!Character.isDigit(s.charAt(i))){
+        for(int i = 0; i<integerString.length(); i++){
+            if(!Character.isDigit(integerString.charAt(i))){
                 count+=1;
             }
         }
-        if (count == s.length()){
+        if (count == integerString.length()){
             return 0;
         }
         //-----------------Base Case-----------------
-        String integerString = s.strip();
+
         int returnInt;
+        int posFlag = 0;
         if(integerString.charAt(0) == '+' ){
             integerString = integerString.substring(1);
         }
+        else{
+            posFlag = 1;
+        }
         for(int i = 0; i < integerString.length(); i++){
-            if((!(Character.isDigit(s.charAt(i)))) && integerString.charAt(i) != '-'){
-                integerString = integerString.substring(integerString.indexOf("-"), i);
+            if(integerString.charAt(i) != '-' && !Character.isDigit(integerString.charAt(i))){
+                integerString = integerString.substring(0, i);
             }
         }
-        System.out.println(integerString);
-        //try{
-        returnInt = Integer.parseInt(integerString);
-        //}
-        //catch (Exception e){
 
-        //}
+        try{
+            returnInt = Integer.parseInt(integerString);
+        }
+        catch(NumberFormatException e){
+            if(posFlag==0){
+                returnInt = (int) ((Math.pow(2,31))-1);
+            }
+            else{
+                returnInt = (int) ((Math.pow(-2,31)));
+            }
+        }
 
         return returnInt;
 
