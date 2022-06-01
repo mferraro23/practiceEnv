c76ba40deafde1b16cb6f85b7237d7efee03d632
Michael Ferraro <mf69386p@pace.edu>
2022-06-01 16:42:15 -0400

    LeetCode Practice (Finished Regular Expression)

--- src/practiceEnv/RegularExpression/RegularExpressionClass.java
+++ src/practiceEnv/RegularExpression/RegularExpressionClass.java
@@ -7,10 +7,12 @@ public class RegularExpressionClass {
         System.out.println(isMatch(s,p));
     }
     public boolean isMatch(String s, String p){
+        // Base Case
         if (p.isEmpty()) return s.isEmpty();
+        // Compare the first letter of s with first letter of p
         boolean first_match = (!s.isEmpty() &&
                 (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
-
+        // Begin Recursive Call
         if (p.length() >= 2 && p.charAt(1) == '*'){
             return (isMatch(s, p.substring(2)) ||
                     (first_match && isMatch(s.substring(1), p)));
