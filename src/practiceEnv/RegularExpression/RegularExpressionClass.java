5b4137ca5f89a1c20eca7adf0e0bdc827951a39c
Michael Ferraro <mf69386p@pace.edu>
2022-06-01 16:41:00 -0400

    LeetCode Practice (Finished Regular Expression)

--- src/practiceEnv/RegularExpression/RegularExpressionClass.java
+++ src/practiceEnv/RegularExpression/RegularExpressionClass.java
@@ -2,16 +2,20 @@ package practiceEnv.RegularExpression;
 
 public class RegularExpressionClass {
     public RegularExpressionClass(){
-        String s = "aa";
+        String s = "ab";
         String p = ".*";
         System.out.println(isMatch(s,p));
     }
     public boolean isMatch(String s, String p){
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == p.charAt(i) || p.charAt(i) == '.'){
-                return true;
-            }
+        if (p.isEmpty()) return s.isEmpty();
+        boolean first_match = (!s.isEmpty() &&
+                (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.'));
+
+        if (p.length() >= 2 && p.charAt(1) == '*'){
+            return (isMatch(s, p.substring(2)) ||
+                    (first_match && isMatch(s.substring(1), p)));
+        } else {
+            return first_match && isMatch(s.substring(1), p.substring(1));
         }
-        return false;
     }
 }
