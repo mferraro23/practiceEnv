9abf7b8020596a18d2f57164ffd1e208cd01fefe
Michael Ferraro <mf69386p@pace.edu>
2022-06-04 23:32:27 -0400

    LeetCode Practice (Finished MinMax)

---
+++ src/practiceEnv/WeeklyChallenge_6_4_2022/MinMaxGame/MinMaxClass.java
@@ -0,0 +1,43 @@
+package practiceEnv.WeeklyChallenge_6_4_2022.MinMaxGame;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MinMaxClass {
+    public MinMaxClass(){
+        int[] nums = {1,3,5,2,4,8,2,2};
+
+        System.out.println(findMinMax(nums));
+    }
+    public int findMinMax(int[] nums) {
+        int n = nums.length;
+        int[] newNums = new int[n/2];
+
+        while(n>1){
+            newNums = new int[n];
+            for(int i = 0; i<n; i++){
+                if(i%2==0){
+                    if(i < n / 2 && i >= 0){
+                        newNums[i] = Math.min(nums[2*i], nums[2 * i +1]);
+                    }
+                }
+                else {
+                    if(i < n / 2 && i >= 0){
+                        newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+                    }
+                }
+            }
+            int[] temp = nums;
+            nums = newNums;
+            newNums = temp;
+            n = n/2;
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+        nums = newNums;
+        return nums[0];
+    }
+}
---
+++ src/practiceEnv/WeeklyChallenge_6_4_2022/MinMaxGame/MinMaxDriver.java
@@ -0,0 +1,7 @@
+package practiceEnv.WeeklyChallenge_6_4_2022.MinMaxGame;
+
+public class MinMaxDriver {
+    public static void main(String[] args) {
+        new MinMaxClass();
+    }
+}
